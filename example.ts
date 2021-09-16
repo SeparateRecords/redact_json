8cf926b42acb966484637f9d6027145212307100
Robert <52195359+SeparateRecords@users.noreply.github.com>
2021-09-16 12:29:34 +1000

    Massively improved example

--- example.ts
+++ example.ts
@@ -14,15 +14,47 @@ const data: JSONFile = {
 	tenItems: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
 };
 
-console.log(data);
-console.log();
-console.log("This output is equivalent to the following options:");
-console.log(
-	"redact --prune=tenItems --shuffle=tenItems,secretArray --preserve=friends",
-);
-console.log();
+console.log("\nconst data =", data);
+
+alert("Continue");
+
+console.log(`----------------
+
+Here's the configuration for how the data will be redacted.
+These are equivalent.
+
+Command-line:
+
+    redact --prune=tenItems \\
+           --shuffle=tenItems,secretArray \\
+           --preserve=friends,tenItems
+
+JavaScript:
+
+    redact(data, {
+      pruneKeys: new Set(["tenItems"]),
+      shuffleKeys: new Set(["tenItems", "secretArray"]),
+      preserveKeys: new Set(["friends", "tenItems"]),
+    })
+
+Arrays under the key 'tenItems' will be shuffled and pruned, but not redacted.
+Arrays under the key 'secretArray' will be shuffled.
+Values under the key 'friends' will not be redacted.
+
+`);
+
+alert("Continue");
+
+console.log("----------------\n");
+
 console.log(redact(data, {
 	pruneKeys: new Set(["tenItems"]),
 	shuffleKeys: new Set(["tenItems", "secretArray"]),
-	preserveKeys: new Set(["friends"]),
+	preserveKeys: new Set(["friends", "tenItems"]),
 }));
+
+console.log(`
+
+If you run this example again, you'll get a different result.
+
+`);
